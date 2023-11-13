57cf97a31c794fde1904a928677b1fa3fef91794
Nahuelrod99 <147419453+Nahuelrod99@users.noreply.github.com>
2023-11-13 22:05:52 +0000

    añado login, register

--- app.js
+++ app.js
@@ -9,5 +9,14 @@ app.use("/public", express.static(publicPath));
 app.listen(7000, () => {console.log("Servidor Corriendo");});
 
 app.get('/', (req,res) => {
-    res.sendFile(path.resolve(__dirname, './views/home.html'))
+    res.sendFile(path.resolve(__dirname, 'views/home.html'))
 });
+app.get("/login", (req, res) => {
+    res.sendFile(path.join(__dirname, "views/login.html"))
+});
+app.get("/register", (req, res) => {
+    res.sendFile(path.join(__dirname, "views/register.html"))
+});
+
+
+
