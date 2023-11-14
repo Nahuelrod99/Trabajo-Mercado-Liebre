de8a65dd09410942c67ba37dea8087ac30a2d597
NahuelRod99 <nahuelr1028@gmail.com>
2023-11-14 18:50:33 -0300

    configuracion para servidor remoto

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ const app = express();
 const publicPath = path.resolve(__dirname, './public');
 app.use("/public", express.static(publicPath));
 
-app.listen(7000, () => {console.log("Servidor Corriendo");});
+let port = process.env.PORT || 7000;
+app.listen(port, () => {console.log("Servidor Corriendo");});
 
 app.get('/', (req,res) => {
     res.sendFile(path.resolve(__dirname, 'views/home.html'))
